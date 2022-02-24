7b3f3360b7b5bf691d224f139a4dbf4d2b24dbae
Anush Rajapakshe <anush.rajapaksha@gmail.com>
2022-02-23 19:09:36 -0800

    Main menu new changes

--- app/src/main/java/com/example/w22_group6_whatthefit/LoginActivity.java
+++ app/src/main/java/com/example/w22_group6_whatthefit/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.example.w22_group6_whatthefit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = findViewById(R.id.toolbarID);
+        setSupportActionBar(toolbar);
+
+
+
+
+
     }
 }
