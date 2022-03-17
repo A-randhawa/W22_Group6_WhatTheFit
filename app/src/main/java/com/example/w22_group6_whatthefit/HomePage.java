abe66284d212e76ca23a8e84a7bd1f51cc4c4483
Anush Rajapakshe <anush.rajapaksha@gmail.com>
2022-03-17 00:26:41 -0700

    main menu changes with videos being added

--- app/src/main/java/com/example/w22_group6_whatthefit/HomePage.java
+++ app/src/main/java/com/example/w22_group6_whatthefit/HomePage.java
@@ -2,7 +2,10 @@ package com.example.w22_group6_whatthefit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        VideoView videoView = findViewById(R.id.videoView1);
+        videoView.setVideoPath("android.resources://"+getPackageName()+"/"+R.raw.workout1);
+       // videoView.setVideoURI(Uri.parse());
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+       // videoView.setOnPreparedListener(mediaPlayer -> videoView.start());
+
     }
 }
