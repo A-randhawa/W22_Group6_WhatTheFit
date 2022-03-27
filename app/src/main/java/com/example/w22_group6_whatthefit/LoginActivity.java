b9443774ceb1921297ea2cec465dc4c25bc125f3
Anush Rajapakshe <anush.rajapaksha@gmail.com>
2022-03-26 22:31:10 -0700

    Login and Sign up backend coding

--- app/src/main/java/com/example/w22_group6_whatthefit/LoginActivity.java
+++ app/src/main/java/com/example/w22_group6_whatthefit/LoginActivity.java
@@ -1,25 +1,103 @@
 package com.example.w22_group6_whatthefit;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText eUsername;
+    EditText ePassword;
+    EditText eAge;
+    EditText eHeight;
+    EditText eWeight;
+    private String URL ="http://192.168.222.1/whatthefit/login.php";
+
+    String username,password;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
+        username= password="";
+        eUsername=findViewById(R.id.txtUsernameSignUp);
+        ePassword=findViewById(R.id.txtPasswordSignUp);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
         Button loginBtn = findViewById(R.id.btnSignUp);
         //click listener
         loginBtn.setOnClickListener((View view) -> {
-            startActivity(new Intent(LoginActivity.this,HomePage.class));
+
+
+            username= eUsername.getText().toString().trim();
+            password=ePassword.getText().toString().trim();
+            if(username !="" && password!="" ) {
+
+            StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.d("res", response);
+
+                    if(response.equals("success")){
+
+                        Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, HomePage.class);
+                        startActivity(intent);
+                        finish();
+                                            }
+                    else if (response.equals("failure")){
+                        Toast.makeText(LoginActivity.this, "Invalid Login Credentials", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }
+                    , new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            }
+
+            ){
+
+
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+
+                    Map<String ,String> map = new HashMap<>();
+                    map.put("username",username);
+
+                    map.put("password",password);
+
+                    return map;
+                }
+            };
+                requestQueue.add(request);;
+            }
+
+
+
+
         });
 
         //text link for Sign up
@@ -30,6 +108,49 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+
+    public void login(View view){
+        username = eUsername.getText().toString().trim();
+        password=ePassword.getText().toString().trim();
+
+        if(username!="" && password!=""){
+            StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("success")) {
+                        Intent intent = new Intent(LoginActivity.this, HomePage.class);
+                        startActivity(intent);
+                        finish();
+                    } else if (response.equals("failure")) {
+                        Toast.makeText(LoginActivity.this, "Incorrect username or Password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(LoginActivity.this,error.toString().trim(),Toast.LENGTH_SHORT);
+                }
+            }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+
+                    Map<String,String> map = new HashMap<>();
+                    map.put("username",username);
+                    map.put("password",password);
+                    return map;
+                }
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(request);
+        }
+        else
+        {
+            Toast.makeText(LoginActivity.this,"Username or password are empty",Toast.LENGTH_SHORT);
+        }
+    }
 
 
 }
--- app/src/main/java/com/example/w22_group6_whatthefit/SignUp.java
+++ app/src/main/java/com/example/w22_group6_whatthefit/SignUp.java
@@ -4,15 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
+
+    EditText eUsername;
+    EditText ePassword;
+    EditText eAge;
+    EditText eHeight;
+    EditText eWeight;
+    TextView tvStatus;
+    Button btnRegister;
+    private String URL ="http://192.168.222.1/whatthefit/register.php";
+    String username,password;
+    String age;
+    String height,weight;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+         requestQueue = Volley.newRequestQueue(getApplicationContext());
+        eUsername =findViewById(R.id.txtUsernameSignUp);
+        ePassword=findViewById(R.id.txtPasswordSignUp);
+        eAge=findViewById(R.id.txtAgeSignUp);
+        eHeight=findViewById(R.id.txtHeightSignUp);
+        eWeight=findViewById(R.id.txtWeightSignUp);
+        btnRegister=findViewById(R.id.btnSignUp);
+        tvStatus=findViewById(R.id.txtViewSignUp);
+        username=password="";
+
+
+         btnRegister.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 save();
+             }
+         });
 
         //text link for Sign up
         TextView txtSignUp = findViewById(R.id.txtViewSignUp);
@@ -20,4 +65,70 @@ public class SignUp extends AppCompatActivity {
             startActivity(new Intent(this,LoginActivity.class));
         });
     }
+
+    public void save() {
+
+        username= eUsername.getText().toString().trim();
+        password=ePassword.getText().toString().trim();
+        age=(eAge.getText().toString().trim());
+        height=(eHeight.getText().toString().trim());
+        weight=(eWeight.getText().toString().trim());
+
+       // requestQueue.add(request);
+        if(username !="" && password!="" && age!="" && height!="" && weight!=""){
+
+          StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+              @Override
+              public void onResponse(String response) {
+                  Log.d("res", response);
+
+                  if(response.equals("success")) {
+                      Toast.makeText(SignUp.this, "User Added Successfully", Toast.LENGTH_SHORT).show();
+                  }
+
+
+                   if (response.equals("failure")) {
+                      Toast.makeText(SignUp.this, "Invalid Login Id/Password", Toast.LENGTH_SHORT).show();
+                  }
+                  clearData();
+
+
+              }
+          }, new Response.ErrorListener() {
+              @Override
+              public void onErrorResponse(VolleyError error) {
+
+              }
+          }){
+
+
+              @Override
+              protected Map<String, String> getParams() throws AuthFailureError {
+
+                  Map<String ,String> map = new HashMap<>();
+                  map.put("username",username);
+                  map.put("height",height);
+                  map.put("weight",weight);
+                  map.put("age",age);
+                  map.put("password",password);
+
+                  return map;
+              }
+          };
+           requestQueue.add(request);
+        }
+
+        }
+
+        public void clearData(){
+
+            eUsername.getText().clear();
+            ePassword.getText().clear();
+            eHeight.getText().clear();
+            eWeight.getText().clear();
+            eAge.getText().clear();
+        }
+
+
+
 }
